e68c6b7db37d5ce3c0c74be887c7afe8c395b658
CiciFromCicily <51415703+cici-xingyu-zheng@users.noreply.github.com>
2019-09-13 20:02:18 -0400

    Update Z00_GetStats.sh

--- ourpipeline/Z00_GetStats.sh
+++ ourpipeline/Z00_GetStats.sh
@@ -3,11 +3,6 @@
 ## directory.  Script expects files ending in '.fastq.gz'.  If the fastq files
 ## are not gzipped, then this code will generate an error
 ##
-##
-## Ron Smith - 2/13/2017
-## rdsmith@email.wm.edu
-##
-##
 ## -----------------------------------------------------------------------------
 
 
@@ -25,12 +20,3 @@ done
 mkdir ../Qual
 mkdir ../NucDist
 
-## Produce the plots
-#for i in ../Stats/*.stats; do
-#    base=${i##*/}
-#    newfile=${base%.stats}_QualBox.png
-#    [ -e "$i" ] && /Users/puzey/homebrew-20170717/local/Cellar/fastx_toolkit/0.0.14/bin/fastq_quality_boxplot_graph.sh -i "$i" -o "../Qual/$newfile" || echo "Somthing went wrong while making the qual plots"
-    
-#    newfile=${base%.stats}_NucDist.png
-#    [ -e "$i" ] && /Users/puzey/homebrew-20170717/local/Cellar/fastx_toolkit/0.0.14/bin/fastx_nucleotide_distribution_graph.sh -i "$i" -o "../NucDist/$newfile" || echo "Somthing went wrong while making the nuc dists"
-#done
